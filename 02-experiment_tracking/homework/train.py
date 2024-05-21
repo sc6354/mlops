4804a8e802e6daa41a1e2e950b4ea8a9e8cbe5f1
Susan Chen <49547982+sc6354@users.noreply.github.com>
2024-05-21 16:31:34 +0000

    Completed HW@ Q3

--- 02-experiment_tracking/homework/train.py
+++ 02-experiment_tracking/homework/train.py
@@ -1,9 +1,10 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 
 def load_pickle(filename: str):
@@ -18,16 +19,18 @@ def load_pickle(filename: str):
     help="Location where the processed NYC taxi trip data was saved"
 )
 def run_train(data_path: str):
+    
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
-
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = root_mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
+    mlflow.sklearn.autolog()
     run_train()
