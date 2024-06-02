60fbe08a97da75b662ba7e8086edba2ea01ec073
sc6354 <49547982+sc6354@users.noreply.github.com>
2024-06-01 19:41:15 -0500

    hw3 q5

--- 03-orchestration/mlops/homework_03/transformers/hw3_transformer.py
+++ 03-orchestration/mlops/homework_03/transformers/hw3_transformer.py
@@ -1,15 +1,14 @@
+
+
 import pandas as pd
 
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
-if 'test' not in globals():
-    from mage_ai.data_preparation.decorators import test
 
 
 @transformer
-def read_dataframe(data):
-    df = pd.read_table(data)
-
+def transform(df: pd.DataFrame, **kwargs):
+    
     df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
     df.duration = df.duration.dt.total_seconds() / 60
 
@@ -21,9 +20,3 @@ def read_dataframe(data):
     return df
 
 
-@test
-def test_output(output, *args) -> None:
-    """
-    Template code for testing the output of the block.
-    """
-    assert output is not None, 'The output is undefined'
