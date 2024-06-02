93ed4f4c753cee3815348fdc202ef8dbf6f707f0
sc6354 <49547982+sc6354@users.noreply.github.com>
2024-06-01 19:09:41 -0500

    added transformer for hw3 q4

--- 03-orchestration/mlops/homework_03/data_loaders/ingest.py
+++ 03-orchestration/mlops/homework_03/data_loaders/ingest.py
@@ -9,22 +9,19 @@ if 'data_loader' not in globals():
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
+link = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet'
+
 @data_loader
 def ingest_files(**kwargs) -> pd.DataFrame:
     dfs: List[pd.DataFrame] = []
 
-    for year, months in [(2023, (1, 3))]:
-        for i in range(*months):
-            response = requests.get(
-                'https://github.com/mage-ai/datasets/raw/master/taxi/green'
-                f'/{year}/{i:02d}.parquet'
-            )
+    response = requests.get(link)
 
-            if response.status_code != 200:
-                raise Exception(response.text)
+    if response.status_code != 200:
+        raise Exception(response.text)
 
-            df = pd.read_parquet(BytesIO(response.content))
-            dfs.append(df)
+    df = pd.read_parquet(BytesIO(response.content))
+    dfs.append(df)
 
     return pd.concat(dfs)
 
